1e40f0155c2d24114e8dba19a5f4cef48b3a913b
ThsloneWolf <hirekaransinghnegi@gmail.com>
2024-02-23 11:58:31 +0530

    Frontend masters react v8

---
+++ 01. Frontend Masters/01. Complete Intro To React V8/08. Component Composition/src/Results.jsx
@@ -0,0 +1,24 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            location={`${pet.city}, ${pet.state}`}
+            images={pet.images}
+            breed={pet.breed}
+            animal={pet.animal}
+            name={pet.name}
+            key={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
