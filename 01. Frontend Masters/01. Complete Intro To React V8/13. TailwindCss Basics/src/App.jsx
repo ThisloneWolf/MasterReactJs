110cc2249da4c02a71c596e4abc7aef5252e2b88
ThisloneWolf <hirekaransinghnegi@gmail.com>
2024-03-04 07:18:17 +0530

    React v8 Tailwind

---
+++ 01. Frontend Masters/01. Complete Intro To React V8/13. TailwindCss Basics/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+// eslint-disable-next-line import/no-unresolved
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+import AdoptedPetContext from "./AdoptedPetContext.js";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  const adoptedPet = useState(null);
+  return (
+    <div
+      className="m-0 p-0"
+      style={{
+        background: "url(https://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+      }}
+    >
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <AdoptedPetContext.Provider value={adoptedPet}>
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 to-red-500 p-7 text-center">
+              <Link to="/" className="text-6xl text-white hover:text-gray-200">
+                Adopt Me!
+              </Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </AdoptedPetContext.Provider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
